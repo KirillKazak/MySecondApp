2a182df867b897cc328a8a53a2787b20aca49374
Kazak Kirill <kazzak.kirill1995@gmail.com>
2021-03-22 01:32:56 +0300

    First commit

---
+++ app/src/main/java/kazak/my_second_app/MainActivity.java
@@ -0,0 +1,22 @@
+package kazak.my_second_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        logOutOurName();
+
+    }
+
+    public void logOutOurName() {
+        Log.d("Key", "Android");
+    }
+}
